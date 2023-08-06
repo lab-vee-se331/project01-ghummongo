a8397be6c0c7111abb6a39914fe117ef5b3f6563
sorawee <veevisorawee@gmail.com>
2023-08-07 02:38:51 +0700

    add colada to observe store

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,21 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+// import Colada Plugin
+import Colada, { PiniaColadaPlugin } from 'colada-plugin';
 import App from './App.vue'
 import router from './router'
 import '@/assets/nprogress.css'; 
 
 
 const app = createApp(App)
+const pinia = createPinia();
 
-app.use(createPinia())
+app.use(pinia);
+pinia.use(PiniaColadaPlugin);
+app.use(Colada);
+
+// app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
